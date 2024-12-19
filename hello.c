a6e8e8e418f9aebeeb7c9cb436d6d00ac3220a52
esther-zhou22 <ezhou2210@gmail.com>
2024-12-19 17:27:36 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,5 +2,6 @@
 
 int main(){
     printf("Hello world !\n");
-    printf("voilà je sais commit et push trop cool !")
+    printf("voilà je sais commit et push trop cool !");
+    printf("là c'est des modifications que je veux pas dans main branch mais juste dans ma branche normalement");
 }
